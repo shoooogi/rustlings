8ee75e5325071ce75e32e7da1146489b1b6e0039
shoooogi <guilhermecomputation@gmail.com>
2026-08-31 23:25:55 -0300

    Fourth day

--- solutions/09_strings/strings1.rs
+++
@@ -1,9 +0,0 @@
-fn current_favorite_color() -> String {
-    // Equivalent to `String::from("blue")`
-    "blue".to_string()
-}
-
-fn main() {
-    let answer = current_favorite_color();
-    println!("My current favorite color is {answer}");
-}
